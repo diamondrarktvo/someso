16c2ed3f923914745cab22d61b917327ea96ee2d
diamondrarktvo <amada.rktvo@gmail.com>
2024-10-10 21:03:07 +0300

    style: configure label of tab navigation

--- src/navigations/TabNavigation.tsx
+++ src/navigations/TabNavigation.tsx
@@ -3,7 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 //LOCAL IMPORT
 import { TabParamList } from "./Types";
-import { Icon } from "_shared";
+import { Icon, Text } from "_shared";
 import { useTheme } from "@shopify/restyle";
 import { Theme } from "_theme";
 import { HomeScreen, ProfileScreen } from "_features";
@@ -12,68 +12,66 @@ const Tab = createBottomTabNavigator<TabParamList>();
 
 //types
 interface TabRouteTypes {
-   name: keyof TabParamList;
-   component: React.FC<unknown>;
-   tabLabel: string;
-   icon: string;
+  name: keyof TabParamList;
+  component: React.FC<unknown>;
+  tabLabel: string;
+  icon: string;
 }
 
 //routes
 const TABROUTES: TabRouteTypes[] = [
-   {
-      name: "home_screen",
-      component: HomeScreen,
-      tabLabel: "Recherche",
-      icon: "home",
-   },
-   {
-      name: "profile_screen",
-      component: ProfileScreen,
-      tabLabel: "Menu",
-      icon: "person-outline",
-   },
+  {
+    name: "home_screen",
+    component: HomeScreen,
+    tabLabel: "Accueil",
+    icon: "home",
+  },
+  {
+    name: "profile_screen",
+    component: ProfileScreen,
+    tabLabel: "Profile",
+    icon: "person-outline",
+  },
 ];
 
 const TabNavigation = () => {
-   const theme = useTheme<Theme>();
-   const { primary, mainForeground, mainBackground } = theme.colors;
-   return (
-      <Tab.Navigator
-         initialRouteName='home_screen'
-         screenOptions={{
-            headerShown: false,
-            tabBarHideOnKeyboard: true,
-            tabBarShowLabel: false,
-            tabBarStyle: [{ backgroundColor: mainBackground }],
-         }}
-      >
-         {TABROUTES.map((route) => (
-            <Tab.Screen
-               key={route.name}
-               name={route.name}
-               component={route.component}
-               options={{
-                  title: route.tabLabel,
-                  tabBarActiveTintColor: primary,
-                  tabBarInactiveTintColor: mainForeground,
-                  /*tabBarLabel: ({ focused }) =>
-              focused ? (
-                <Text style={{ color: "white" }}>{route.tabLabel}</Text>
-              ) : (
-                ""
-              ),*/
-                  tabBarIcon: ({ focused, color }) => (
-                     <Icon
-                        name={route.icon}
-                        color={color}
-                        size={focused ? 32 : 22}
-                     />
-                  ),
-               }}
-            />
-         ))}
-      </Tab.Navigator>
-   );
+  const theme = useTheme<Theme>();
+  const { primary, mainForeground, mainBackground } = theme.colors;
+  return (
+    <Tab.Navigator
+      initialRouteName="home_screen"
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: true,
+        tabBarStyle: [{ backgroundColor: mainBackground }],
+      }}
+    >
+      {TABROUTES.map((route) => (
+        <Tab.Screen
+          key={route.name}
+          name={route.name}
+          component={route.component}
+          options={{
+            title: route.tabLabel,
+            tabBarActiveTintColor: primary,
+            tabBarInactiveTintColor: mainForeground,
+            tabBarLabel: ({ focused }) => (
+              <Text
+                variant={"tertiaryBold"}
+                color={focused ? "primary" : "black"}
+              >
+                {route.tabLabel}
+              </Text>
+            ),
+            tabBarIcon: ({ focused, color }) => (
+              <Icon name={route.icon} color={color} size={focused ? 32 : 22} />
+            ),
+          }}
+        />
+      ))}
+    </Tab.Navigator>
+  );
 };
 
 export default TabNavigation;
